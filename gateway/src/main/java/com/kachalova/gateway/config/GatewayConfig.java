e181410f3b9d7233c1de10ad3b7a3f286d36a01e
j-kachalova <62459071+j-kachalova@users.noreply.github.com>
2025-05-18 23:47:48 +0400

    add gateway

---
+++ gateway/src/main/java/com/kachalova/gateway/config/GatewayConfig.java
@@ -0,0 +1,25 @@
+package com.kachalova.gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                // Маршрут для RuleSet Service
+                .route("ruleset_route", r -> r.path("/api/rulesets/**")
+                        .uri("http://ruleset:8082"))  // Статический URI для Ruleset Service
+                // Маршрут для Job Service
+                .route("job_route", r -> r.path("/api/jobs/**")
+                        .uri("http://job-service:8083"))  // Статический URI для Job Service
+                // Маршрут для File Processing Service
+                .route("file_route", r -> r.path("/files/**")
+                        .uri("http://file-processing:8084"))  // Статический URI для File Processing Service
+                .build();
+    }
+}
